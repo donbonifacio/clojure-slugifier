0d2e16fdabb4a68dcecbc5a7ebd108a7f7fc21e7
Pedro Santos <donbonifacio@gmail.com>
2014-07-18 15:20:01 +0100

    Simplify testing

--- test/clojure_slugifier/core_test.clj
+++ test/clojure_slugifier/core_test.clj
@@ -2,18 +2,12 @@
   (:require [clojure.test :refer :all]
             [clojure-slugifier.core :refer :all]))
 
-(deftest basic-slugify
-  (testing "basic slugify usage"
-    (is (= "pedro" (slugify "pedro")))))
+(testing "handles uncomon chars"
+  (is (= "andre-da-conceicao" (slugify "André da Conceição"))))
 
-(deftest dashes-for-spaces
-  (testing "dashes for spaces"
-    (is (= "pedro-santos" (slugify "pedro santos")))))
+(testing "dashes for spaces"
+  (is (= "pedro-santos" (slugify "pedro santos"))))
 
-(deftest undercase
-  (testing "makes lowercase"
-    (is (= "pedro-santos" (slugify "Pedro Santos")))))
+(testing "lower cases"
+  (is (= "pedro-santos" (slugify "Pedro Santos"))))
 
-(deftest undercase
-  (testing "handles uncomon chars"
-    (is (= "andre-da-conceicao" (slugify "André da Conceição")))))
