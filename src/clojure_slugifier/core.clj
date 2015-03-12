1ea1ad8b2fa624a66c41f5d7527dca542b4ae726
Pedro Santos <donbonifacio@gmail.com>
2015-03-12 21:14:14 +0000

    Minor refactor

--- src/clojure_slugifier/core.clj
+++ src/clojure_slugifier/core.clj
@@ -13,14 +13,11 @@
   (let [normalized (java.text.Normalizer/normalize raw java.text.Normalizer$Form/NFKD)]
     (.replaceAll normalized "\\p{InCombiningDiacriticalMarks}+"  "")))
 
-(defn- slugifier
-  []
-  (comp spaces-for-dashes
-        decompose
-        lowercase))
-
 (defn slugify
   "Slugifies a given string"
   [raw]
-  ((slugifier) raw))
+  (-> raw
+      (lowercase)
+      (decompose)
+      (spaces-for-dashes)))
 
--- test/clojure_slugifier/core_test.clj
+++ test/clojure_slugifier/core_test.clj
@@ -2,12 +2,13 @@
   (:require [clojure.test :refer :all]
             [clojure-slugifier.core :refer :all]))
 
-(testing "handles uncomon chars"
-  (is (= "andre-da-conceicao" (slugify "André da Conceição"))))
+(deftest slugifier
+  (testing "handles uncomon chars"
+    (is (= "andre-da-conceicao" (slugify "André da Conceição"))))
 
-(testing "dashes for spaces"
-  (is (= "pedro-santos" (slugify "pedro santos"))))
+  (testing "dashes for spaces"
+    (is (= "pedro-santos" (slugify "pedro santos"))))
 
-(testing "lower cases"
-  (is (= "pedro-santos" (slugify "Pedro Santos"))))
+  (testing "lower cases"
+    (is (= "pedro-santos" (slugify "Pedro Santos")))))
 
