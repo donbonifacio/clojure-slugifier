02197627de4c77bb30447790fbf641a7ca1f8a05
Pedro Santos <donbonifacio@gmail.com>
2015-03-27 11:02:52 +0000

    Test additional clojure versions

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "https://github.com/donbonifacio/clojure-slugifier"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
+  :aliases {"all" ["with-profile" "dev:1.5:1.7" "test"]})
