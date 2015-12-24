b9c93a4796eb713954bf16f055796fb2c936518a
Pedro Santos <donbonifacio@gmail.com>
2015-12-24 15:46:22 +0000

    Bump version  to 0.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clojure-slugifier "0.1"
+(defproject clojure-slugifier "0.2.0"
   :description "slugifies strings"
   :url "https://github.com/donbonifacio/clojure-slugifier"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
